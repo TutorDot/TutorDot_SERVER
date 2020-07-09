b1fd01fd25248016c020322cc1e3a57b08870b42
Um seoyoung <tjdud0123@naver.com>
2020-07-09 23:40:48 +0900

    보일러플레이트 1차

---
+++ workspace/routes/user.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+const UserController = require('../controllers/user');
+const AuthMiddleware = require('../middlewares/auth');
+const upload = require('../modules/multer')
+const imageController = require('../controllers/image.js')
+
+router.post('/signup', UserController.signup);
+router.post('/signin', UserController.signin);
+
+/* 
+    ✔️ update profile
+    METHOD : POST
+    URI : localhost:3000/user/profile
+    REQUEST HEADER : JWT
+    REQUEST BODY : ⭐️image file ⭐️
+    RESPONSE DATA : user profile
+*/
+
+router.post('/profile', AuthMiddleware.checkToken, upload.single('profile'), UserController.updateProfile);
+router.post('/selfies', AuthMiddleware.checkToken, upload.array('images', 4), imageController.array);
+module.exports = router;
