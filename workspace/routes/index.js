4e7c4c4a8fcfad30e70ebf798b1a4e5f23cd0148
Um seoyoung <tjdud0123@naver.com>
2020-07-15 02:07:03 +0900

    노드몬설치, 풀

--- workspace/routes/index.js
+++ workspace/routes/index.js
@@ -3,9 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', {
-    title: 'TutorDot'
-  });
+    res.render('index', {
+        title: 'TutorDot'
+    });
+});
 
 // 계정
 router.use('/user', require('./user'));
