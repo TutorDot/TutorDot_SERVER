dd0b2a152307bce5f619a4fa9facee8924241bcc
dldbdud314 <dldbdud314@naver.com>
2020-11-14 20:04:50 +0900

    notice 변경

--- workspace/routes/index.js
+++ workspace/routes/index.js
@@ -24,6 +24,31 @@ router.use("/diary", require("./diary"));
 // 알림
 router.use("/notice", require("./notice"));
 
+var admin = require("firebase-admin");
+
+var serviceAccount = require("/Users/yooyounglee/Documents/tutordot-ff6d6-firebase-adminsdk-paa4m-95db774518.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://tutordot-ff6d6.firebaseio.com"
+});
+
+var registrationToken = "cUbK4zaSSxm710dyYu3OQn:APA91bG1j90tt1RmrK7RtjBGd9SPRuL1vxhDiQZSTxFTx8DkD4KNz1XMY59JHJLY_ESw1jRlXzkgTLm7WvZvWKxDFztv_RbZ58x3ryeN5jyIpr3d2h9COTrriTlEngxY98pptMt6yDqB"; 
+
+var payload = {
+  notification: {
+    title: "일지 업데이트",
+    body: "수업 일지가 업데이트 되었습니다!"
+  }
+};
+
+admin.messaging().sendToDevice(registrationToken, payload).then(function(response){
+  console.log("Successfully sent: ", response);
+})
+.catch(function(error){
+  console.log("Error sending message: ", error);
+});
+
 //소셜로그인
 router.use("/login",require("./login.js"));
 
