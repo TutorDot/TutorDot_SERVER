f5b0b905c2fef9d75fbd53870c3a0bebd9b17861
seeheee <53335160+seeheee@users.noreply.github.com>
2020-11-14 18:12:12 +0900

    commit

--- workspace/routes/notice.js
+++ workspace/routes/notice.js
@@ -9,4 +9,7 @@ router.get('/', AuthMiddleware.checkToken, NoticeController.getAll);
 // 알림 상세 목록 조회
 router.get('/:lid', AuthMiddleware.checkToken, NoticeController.getNoticeId);
 
+// 수업일지가 추가되었습니다.
+router.get('/addlecture', AuthMiddleware.checkToken, NoticeController.getNoticeId);
+
 module.exports = router;
