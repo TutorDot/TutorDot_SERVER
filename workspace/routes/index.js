1290a80cb51e3724afe429b655af299a703375a0
seeheee <53335160+seeheee@users.noreply.github.com>
2020-11-23 23:26:46 +0900

    firebase error

--- workspace/routes/index.js
+++ workspace/routes/index.js
@@ -27,32 +27,32 @@ router.use("/question", require("./question"));
 // 알림
 router.use("/notice", require("./notice"));
 
-var admin = require("firebase-admin");
+// var admin = require("firebase-admin");
 
-var serviceAccount = require("C:/Users/권세희/Desktop/tutordot-ff6d6-firebase-adminsdk-paa4m-95db774518.json");
+// var serviceAccount = require("C:/Users/권세희/Desktop/tutordot-ff6d6-firebase-adminsdk-paa4m-95db774518.json");
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://tutordot-ff6d6.firebaseio.com"
-});
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+//   databaseURL: "https://tutordot-ff6d6.firebaseio.com"
+// });
 
-var registrationToken = "cUbK4zaSSxm710dyYu3OQn:APA91bG1j90tt1RmrK7RtjBGd9SPRuL1vxhDiQZSTxFTx8DkD4KNz1XMY59JHJLY_ESw1jRlXzkgTLm7WvZvWKxDFztv_RbZ58x3ryeN5jyIpr3d2h9COTrriTlEngxY98pptMt6yDqB"; 
+// var registrationToken = "cUbK4zaSSxm710dyYu3OQn:APA91bG1j90tt1RmrK7RtjBGd9SPRuL1vxhDiQZSTxFTx8DkD4KNz1XMY59JHJLY_ESw1jRlXzkgTLm7WvZvWKxDFztv_RbZ58x3ryeN5jyIpr3d2h9COTrriTlEngxY98pptMt6yDqB"; 
 
-var payload = {
-  notification: {
-    title: "일지 업데이트",
-    body: "수업 일지가 업데이트 되었습니다!"
-  }
-};
+// var payload = {
+//   notification: {
+//     title: "일지 업데이트",
+//     body: "수업 일지가 업데이트 되었습니다!"
+//   }
+// };
 
-admin.messaging().sendToDevice(registrationToken, payload).then(function(response){
-  console.log("Successfully sent: ", response);
-})
-.catch(function(error){
-  console.log("Error sending message: ", error);
-});
+// admin.messaging().sendToDevice(registrationToken, payload).then(function(response){
+//   console.log("Successfully sent: ", response);
+// })
+// .catch(function(error){
+//   console.log("Error sending message: ", error);
+// });
 
-//소셜로그인
-router.use("/login",require("./login.js"));
+// //소셜로그인
+// router.use("/login",require("./login.js"));
 
 module.exports = router;
