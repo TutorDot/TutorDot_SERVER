6b30761640a2ed8fb116f22778d0e94798527dd0
dldbdud314 <dldbdud314@naver.com>
2020-11-26 19:53:44 +0900

    일정 추가 일정 삭제 버그 수정

--- workspace/routes/index.js
+++ workspace/routes/index.js
@@ -28,30 +28,30 @@ router.use("/question", require("./question"));
 // 알림
 router.use("/notice", require("./notice"));
 
-var admin = require("firebase-admin");
+// var admin = require("firebase-admin");
 
-var serviceAccount = firebaseKey;
+// var serviceAccount = firebaseKey;
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://tutordot-ff6d6.firebaseio.com"
-});
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+//   databaseURL: "https://tutordot-ff6d6.firebaseio.com"
+// });
 
-var registrationToken = "cUbK4zaSSxm710dyYu3OQn:APA91bG1j90tt1RmrK7RtjBGd9SPRuL1vxhDiQZSTxFTx8DkD4KNz1XMY59JHJLY_ESw1jRlXzkgTLm7WvZvWKxDFztv_RbZ58x3ryeN5jyIpr3d2h9COTrriTlEngxY98pptMt6yDqB"; 
+// var registrationToken = "e4A0TUIYSIa6S5pXoAFW0U:APA91bH39_73kpAkfUOZg7659u-f9NoPAAdxItWlIRg8BrB2-GOrMHIeN4M2qwKv0qc2ill3W6XTMaW44U4P2b2DLkn8_sJi_lqesXtd1b_EQ_A627R8JtVpb1sbZiHD3DmWvqGG3_R2"; 
 
-var payload = {
-  notification: {
-    title: "일지 업데이트",
-    body: "수업 일지가 업데이트 되었습니다!"
-  }
-};
+// var payload = {
+//   notification: {
+//     title: "일지 업데이트",
+//     body: "수업 일지가 업데이트 되었습니다!"
+//   }
+// };
 
-admin.messaging().sendToDevice(registrationToken, payload).then(function(response){
-  console.log("Successfully sent: ", response);
-})
-.catch(function(error){
-  console.log("Error sending message: ", error);
-});
+// admin.messaging().sendToDevice(registrationToken, payload).then(function(response){
+//   console.log("Successfully sent: ", response);
+// })
+// .catch(function(error){
+//   console.log("Error sending message: ", error);
+// });
 
 //소셜로그인
 router.use("/login",require("./login.js"));
