85cde1f381066cd80928d8f3fbabf86b9dd8d9c9
seeheee <53335160+seeheee@users.noreply.github.com>
2020-11-28 23:53:30 +0900

    firebase

--- workspace/routes/index.js
+++ workspace/routes/index.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var router = express.Router();
-//const firebaseKey = require('../config/firebase.json');
+
 
 /* GET home page. */
 
@@ -27,31 +27,6 @@ router.use("/question", require("./question"));
 
 // 알림
 router.use("/notice", require("./notice"));
-
-// var admin = require("firebase-admin");
-
-// var serviceAccount = firebaseKey;
-
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount),
-//   databaseURL: "https://tutordot-ff6d6.firebaseio.com"
-// });
-
-// var registrationToken = "e4A0TUIYSIa6S5pXoAFW0U:APA91bH39_73kpAkfUOZg7659u-f9NoPAAdxItWlIRg8BrB2-GOrMHIeN4M2qwKv0qc2ill3W6XTMaW44U4P2b2DLkn8_sJi_lqesXtd1b_EQ_A627R8JtVpb1sbZiHD3DmWvqGG3_R2"; 
-
-// var payload = {
-//   notification: {
-//     title: "일지 업데이트",
-//     body: "수업 일지가 업데이트 되었습니다!"
-//   }
-// };
-
-// admin.messaging().sendToDevice(registrationToken, payload).then(function(response){
-//   console.log("Successfully sent: ", response);
-// })
-// .catch(function(error){
-//   console.log("Error sending message: ", error);
-// });
 
 //소셜로그인
 router.use("/login",require("./login.js"));
--- workspace/routes/notice.js
+++ workspace/routes/notice.js
@@ -1,15 +1,22 @@
 const express = require("express");
 const router = express.Router();
-const NoticeController = require("../controllers/notice");
+const fireBaseController = require("../controllers/firebase");
 const AuthMiddleware = require("../middlewares/auth");
 
-// 알림 전체 목록 조회
-router.get('/', AuthMiddleware.checkToken, NoticeController.getAll);
+// // 알림 전체 목록 조회
+// router.get('/', AuthMiddleware.checkToken, NoticeController.getAll);
 
-// 알림 상세 목록 조회
-router.get('/:lid', AuthMiddleware.checkToken, NoticeController.getNoticeId);
+// // 알림 상세 목록 조회
+// router.get('/:lid', AuthMiddleware.checkToken, NoticeController.getNoticeId);
 
-// 수업일지가 추가되었습니다.
-router.get('/addlecture', AuthMiddleware.checkToken, NoticeController.getNoticeId);
+// // 수업일지가 추가되었습니다.
+// router.get('/addlecture', AuthMiddleware.checkToken, NoticeController.getNoticeId);
+
+// 파이어베이스 코드 집어넣기
+router.post('/insertCode', AuthMiddleware.checkToken, fireBaseController.insertCode);
+
+// 파이어베이스 수업 일지 업데이트 푸시 알림 보내기
+router.post('/updateDiary',AuthMiddleware.checkToken,  fireBaseController.updateDiary);
+
 
 module.exports = router;
