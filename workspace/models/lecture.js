c8918122beb799ec5620d3dd104d4cf126e8aa30
SeoYng <tjdud0123@naver.com>
2020-12-01 14:26:45 +0900

    네임 내려오게 다시 수정

--- workspace/models/lecture.js
+++ workspace/models/lecture.js
@@ -169,7 +169,7 @@ const lecture = {
 
     /* 수업 상세 조회  get : [ /lecture/:lid ] */
     getLectureById: async (userIdx, lectureId) => {
-        const query = `SELECT lectureId, color, orgLocation, bank, accountNo, depositCycle, price, userName, role, intro, profileUrl 
+        const query = `SELECT lectureName, color, orgLocation, bank, accountNo, depositCycle, price, userName, role, intro, profileUrl 
         FROM lecture as l INNER JOIN connect as c on c.lecture_lectureId = l.lectureId 
         INNER JOIN user as u on c.user_userId = u.userId WHERE l.lectureId = ${lectureId} AND role="tutor"`
         try {
